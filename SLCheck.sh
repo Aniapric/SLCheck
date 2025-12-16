6f777a3779cbaaf8c4c5120912a055208a9ba2fe
Pricop Ania-Stefania <pricania@icloud.com>
2025-12-16 14:58:50 +0200

    Initial commit - SLCheck shell script

---
+++ SLCheck.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+FOLLOW_SYMLINKS=false
+TARGET_DIR=""
+
+for arg in "$@"; do
+   if [ "$arg" == ""--follow-symlinks" ]; then
+     FOLLOW_SYMLINKS=true
+   elif [ -z "$TARGET_DIR" ]; then
+     TARGET_DIR="$arg"
+   fi
+done
+
+if [ -z "$TARGET_DIR" ]; then
+  echo "Utilizare: $0 [director] [--follow-symlinks]"
+  exit 1
+fi
+
+process_directory(){
+  local current_dir="$1"
+  if [ ! -r "$current_dir" ]; then
+    echo "Avertisment: Eroare citire director $current_dir "
+  return
+  fi
+  for file in "$current_dir"/*; do
+    if [ ! -e "$file" ] && [ ! -L "$file" ]; then continue; fi
+
+    if [  -L "$file" ]; then
+       if [ ! -e "$file" ]; then
+           echo " [BROKEN LINK] $file"
+       else
+           if [ -d "$file" ] && ["$FOLLOW_SYMLINKS" = true ]; then
+              process_directory "$file"
+           fi
+       fi
+
+    elif [ -d "$file" ]; then
+        process_directory "$file"
+    fi
+
+  done
+}
+
+echo "Analizez directorul: $TARGET_DIR"
+process_directory "$TARGET_DIR"
